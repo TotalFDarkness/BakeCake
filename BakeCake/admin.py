0b69a4f55901b01d1d37dfd0c5b8059c4dc2a99e
VladislavVenin <d4spda@yandex.ru>
2026-03-26 23:07:04 +1100

    add admin

--- BakeCake/admin.py
+++ BakeCake/admin.py
@@ -1,3 +1,66 @@
 from django.contrib import admin
+from .models import (
+    OrderedCake,
+    Order,
+    Cake,
+    Client,
+    Layer,
+    Shape,
+    Topping,
+    Berry,
+    Inscription,
+    Decor
+)
 
-# Register your models here.
+
+@admin.register(OrderedCake)
+class OrderedCakeAdmin(admin.ModelAdmin):
+    list_display = ['order', 'cake', 'quantity']
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['client', 'order_price', 'status']
+    list_filter = ['status']
+    list_editable = ['status']
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ['fio', 'phone', 'address']
+
+
+@admin.register(Cake)
+class CakeAdmin(admin.ModelAdmin):
+    list_display = ['picture', 'title']
+    list_display_links = ['picture', 'title']
+
+
+@admin.register(Layer)
+class LayerAdmin(admin.ModelAdmin):
+    list_display = ['quantity', 'price']
+
+
+@admin.register(Shape)
+class ShapeAdmin(admin.ModelAdmin):
+    list_display = ['shape', 'price']
+
+
+@admin.register(Topping)
+class ToppingAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price']
+
+
+@admin.register(Berry)
+class BerryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price']
+
+
+@admin.register(Inscription)
+class InscriptionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price']
+
+
+@admin.register(Decor)
+class DecorAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price']
